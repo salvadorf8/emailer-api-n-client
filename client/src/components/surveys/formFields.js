78a26196cef90d37f7134021b7c7d7f2b0383468
salvadorf8 <aztecsalvador@gmail.com>
2019-09-28 21:32:34 -0500

    SF - updated some verbiage

--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -1,5 +1,5 @@
 export default [
-    { label: 'Title', name: 'title' },
+    { label: 'Give this record a title', name: 'title' },
     { label: 'Year, Make, Model, and engine', name: 'subject' },
     { label: 'Detailed description of what you are looking for', name: 'body' },
     { label: 'Recipient List', name: 'recipients' }
